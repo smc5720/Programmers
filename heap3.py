9d12ec82747b4f0e939f416f2b91aa7e265207f6
unknown <smc5720@gmail.com>
2021-04-25 11:57:28 +0900

    20210425

--- heap3.py
+++ heap3.py
@@ -3,8 +3,25 @@ import heapq
 def solution(operations):
     answer = []
     heap = []
+
     for i in operations:
         if i[0] == "I":
-
+            heapq.heappush(heap, int(i[2:]))
+        else:
+            if heap:
+                if i[2] == "-":
+                    heapq.heappop(heap)
+                else:
+                    heap.sort()
+                    heap.pop()
+                    heapq.heapify(heap)
+    
+    if len(heap) > 0:
+        heap.sort()
+        answer.append(heap[-1])
+        answer.append(heap[0])
+    else:
+        answer.append(0)
+        answer.append(0)
 
     return answer
