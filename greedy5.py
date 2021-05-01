059ee9dd22e152c25c80131397bf5c1725422f78
unknown <smc5720@gmail.com>
2021-05-01 13:03:49 +0900

    20210501

---
+++ greedy5.py
@@ -0,0 +1,26 @@
+def solution(n, costs):
+    answer = 0
+    costs.sort(key=lambda x:x[2])
+    parent = [0] * n
+
+    for i in range(0, n):
+        parent[i] = i
+
+    def get_parent(num):
+        if num == parent[num]:
+            return num
+        else:
+            return get_parent(parent[num])
+
+    def set_parent(a, b):
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+    
+    for i in costs:
+        if get_parent(i[0]) != get_parent(i[1]):
+            set_parent(i[0], i[1])
+            answer += i[2]
+
+    return answer
