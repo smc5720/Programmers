029ad2f1fbe28de2d4acc03c7ddfe99a582faba7
smc5720 <smc5720@gmail.com>
2021-05-09 19:54:27 +0900

    20210509

---
+++ dfsNbfs2.py
@@ -0,0 +1,17 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+
+    def dfs(n):
+        for i in range(0, len(computers[n])):
+            if visited[i] == False and computers[n][i] == 1:
+                visited[i] = True
+                dfs(i)
+
+    for i in range(0, n):
+        if visited[i] == False:
+            visited[i] = True
+            answer += 1
+            dfs(i)
+
+    return answer
