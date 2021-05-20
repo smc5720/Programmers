aa3d281fd98ddc261adf4bf64ebc3e28d1b66ccf
smc5720 <smc5720@gmail.com>
2021-05-20 13:23:42 +0900

    20210520

--- 실패율.py
+++ 실패율.py
@@ -4,13 +4,26 @@ def solution(N, stages):
     answer = []
     stage_dict = defaultdict(int)
     arr = []
+    people = len(stages)
 
     for i in stages:
         if i <= N:
             stage_dict[i] += 1
     
-    for i in range(1, N + 1):
-        arr.append([len(stages), i])
+    for i in range(1, N + 2):
+        arr.append([people, i])
+        people -= stage_dict[i]
 
-    print(stage_dict, arr)
+    for i in range(0, N):
+        if arr[i][0] != 0:
+            arr[i][0] = -1 * (arr[i][0] - arr[i + 1][0]) / arr[i][0]
+        else:
+            arr[i][0] = 0
+    
+    arr.pop()
+    arr.sort()
+
+    for i in arr:
+        answer.append(i[1])
+
     return answer
