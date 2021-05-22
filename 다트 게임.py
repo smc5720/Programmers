a805a7c73c3826eaba61fc85e2f450b481d945ff
smc5720 <smc5720@gmail.com>
2021-05-22 16:34:53 +0900

    20210522

--- 다트 게임.py
+++ 다트 게임.py
@@ -1,21 +1,21 @@
 def solution(dartResult):
     answer = 0
     stack = []
-    dartResult = list(dartResult)
+    dartResult = list(dartResult[::-1])
 
     while dartResult:
         tmp = dartResult.pop()
-        if tmp != '0':
-            if '0' < tmp and tmp <= '9': 
+        if tmp != '1':
+            if '0' <= tmp and tmp <= '9': 
                 stack.append(int(tmp))
             else:
                 stack.append(tmp)
         else:
-            if dartResult[-1] == '1':
+            if dartResult[-1] == '0':
                 dartResult.pop()
                 stack.append(10)
             else:
-                stack.append(0)
+                stack.append(1)
     
     star_cnt = 0
     shop_cnt = 0
@@ -28,17 +28,19 @@ def solution(dartResult):
             shop_cnt += 1
         elif tmp == 'T' or tmp == 'D' or tmp == 'S':
             num = stack.pop()
-            print(num)
             if tmp == 'T':
                 num = num ** 3
             if tmp == 'D':
                 num = num ** 2
             if star_cnt > 0:
+                if star_cnt > 2:
+                    num *= 2
+                    star_cnt -= 1
                 num *= 2
                 star_cnt -= 1
             if shop_cnt > 0:
                 num *= -1
                 shop_cnt -= 1
-            #answer += num
+            answer += num
 
     return answer
