9ae7cc0193b5c03496c5b1abb4925c419f28f003
Mincheol Seo <smc503@naver.com>
2021-06-27 14:41:23 +0900

    202106267

--- 카드 짝 맞추기.py
+++ 카드 짝 맞추기.py
@@ -1,135 +1,126 @@
-from itertools import permutations
+from collections import defaultdict
 from collections import deque
+from itertools import permutations
 
-# 못 푼 문제
+answer = float("inf")
 
-size = 4
-myboard = [[] for i in range(4)]
-card_pos = {}
-d = [[0,1], [1,0], [0,-1], [-1,0]]
-INF = int(1e4)
-answer = INF
-orders = []
+def visitable(y, x):
+    return 0 <= x < 4 and 0 <= y < 4
 
-# 전역 변수를 이용한 보드(myboard), 카드 2장의 위치(card_pos) 초기화 
-# 지우는 순서에 대한 순열(orders) 초기화
-# card_pos 예시: card_pos[1] = [[0,0], [1,2]] // 카드 1은 보드의 [0,0], [1,2]에 존재 
-def init(board):
-    global myboard, card_pos, orders
-    for i in range(size):
-        for j in range(size):
-            if board[i][j] != 0:
-                card = board[i][j]
-                if card not in card_pos.keys(): card_pos[card] = [[i,j]]
-                else: card_pos[card].append([i,j])
+# 카드의 종류를 넣으면 해당 카드를 보드에서 제거한다.
+def remove_card(board, card_pos, card_kind):
+    for i in range(2):
+        y, x = card_pos[card_kind][i]
+        board[y][x] = 0
 
-            myboard[i].append(board[i][j])
-    
-    orders = [key for key in card_pos.keys()]
-    orders = list(permutations(orders))
-    
-# 이동한 결과가 보드 범위내 있는지 판단하는 함수            
-def isin(y,x):
-    if -1<y<size:
-        if -1<x<size: return True
-        
-    return False
 
-# ctrl + 방향키
-def move(y, x, mv):
-    global myboard
+# 카드의 종류를 넣으면 해당 카드를 보드에서 복구한다.
+def restore_card(board, card_pos, card_kind):
+    for i in range(2):
+        y, x = card_pos[card_kind][i]
+        board[y][x] = card_kind
+
+
+def ctrl_move(board, y, x, direc):
+    left, right, up, down = 0, 1, 2, 3
+    y_dir = [0, 0, -1, 1]
+    x_dir = [-1, 1, 0, 0]
     ny, nx = y, x
-
     while True:
-        _ny = ny + mv[0]
-        _nx = nx + mv[1]
-        if not isin(_ny, _nx): return ny, nx
-        if myboard[_ny][_nx] != 0: return _ny, _nx
-            
-        ny = _ny
-        nx = _nx
+        tmp_y = ny + y_dir[direc]
+        tmp_x = nx + x_dir[direc]
+        if not visitable(tmp_y, tmp_x):
+            return ny, nx
+        if board[tmp_y][tmp_x] != 0:
+            return tmp_y, tmp_x
+        ny, nx = tmp_y, tmp_x
 
-# 카드 1장을 찾을 때 나오는 거리를 반환하는 함수(목표 위치도 반환함)
-# 시작 위치: myboard[sy, sx], 찾아야 할 위치: myboard[ey, ex] 
-def bfs(sy, sx, ey, ex):
-    if [sy, sx] == [ey, ex]: return sy, sx, 1
-    global myboard
-    q = []
-    q = deque(q)
-    table = [[0 for j in range(size)] for i in range(size)]
-    visit = [[False for j in range(size)] for i in range(size)]
-    q.append([sy, sx])
-    visit[sy][sx] = True
 
-    while q:
-        y, x = q.popleft()
+def bfs(board, start_y, start_x, des_y, des_x):
+    if [start_y, start_x] == [des_y, des_x]:
+        return start_y, start_x, 1
+    # table은 현재 위치부터 해당 좌표까지의 최소 이동 횟수를 저장한다.
+    table = [[0 for _ in range(4)] for a in range(4)]
+    visited = [[False for _ in range(4)] for a in range(4)]
+    queue = deque()
+    queue.append([start_y, start_x])
+    visited[start_y][start_x] = True
+    y_dir = [0, 0, -1, 1]
+    x_dir = [-1, 1, 0, 0]
 
+    while queue:
+        y, x = queue.popleft()
         for i in range(4):
-            ny = y + d[i][0]
-            nx = x + d[i][1]
+            # 먼저 상하좌우로 움직이는 경우이다.
+            now_y, now_x = y + y_dir[i], x + x_dir[i]
+            # 방문 가능한 곳이라면
+            if visitable(now_y, now_x) and not visited[now_y][now_x]:
+                # 방문 여부를 True로 설정하고
+                visited[now_y][now_x] = True
+                # 이동 횟수를 저장한다.
+                table[now_y][now_x] = table[y][x] + 1
+                if [now_y, now_x] == [des_y, des_x]:
+                    return now_y, now_x, table[now_y][now_x] + 1
+                queue.append([now_y, now_x])
 
-            if isin(ny, nx):
-                if not visit[ny][nx]:
-                    visit[ny][nx] = True
-                    table[ny][nx] = table[y][x] + 1
-                    if [ny,nx] == [ey,ex]:
-                        return ny, nx, table[ny][nx] + 1
+            # 이후에는 ctrl 이동을 고려한다.
+            now_y, now_x = ctrl_move(board, y, x, i)
+            # 방문 가능한 곳이라면
+            if not visited[now_y][now_x]:
+                # 방문 여부를 True로 설정하고
+                visited[now_y][now_x] = True
+                # 이동 횟수를 저장한다.
+                table[now_y][now_x] = table[y][x] + 1
+                if [now_y, now_x] == [des_y, des_x]:
+                    return now_y, now_x, table[now_y][now_x] + 1
+                queue.append([now_y, now_x])
+    return start_y, start_x, float("inf")
 
-                    q.append([ny, nx])
 
-            ny, nx = move(y, x, d[i])
-
-            if not visit[ny][nx]:
-                visit[ny][nx] = True      
-                table[ny][nx] = table[y][x] + 1
-                if [ny,nx] == [ey,ex]:
-                    return ny, nx, table[ny][nx] + 1
-
-                q.append([ny, nx])
-
-    return sy, sx, INF
-
-# 찾은 2장의 카드를 myboard에서 지워주는 함수           
-def remove(card):
-    global myboard, card_pos
-    for y, x in card_pos[card]: myboard[y][x] = 0
-
-# 지워진 2장의 카드를 myboard에서 복원해주는 함수
-def restore(card):
-    global myboard, card_pos
-    for y, x in card_pos[card]: myboard[y][x] = card
-
-# backtracking
-def backtrack(sy, sx, k, m, count):
-    global orders, myboard, answer, card_pos
-
-    if k == len(card_pos.keys()):
-        if answer > count: answer = count
+# rmv_card_num: 없어진 카드 쌍의 수를 의미한다.
+# ord_idx: 현재 사용할 orders idx를 의미한다.
+def backtracking(board, card_pos, orders, start_y, start_x, rmv_card_num, ord_idx, count):
+    global answer
+    if rmv_card_num == len(card_pos.keys()):
+        answer = min(answer, count)
         return
+    # 현재 제거하려는 카드의 종류를 저장한다.
+    card_kind = orders[ord_idx][rmv_card_num]
 
-    card = orders[m][k]
-    left_y, left_x = card_pos[card][0][0], card_pos[card][0][1]
-    right_y, right_x = card_pos[card][1][0], card_pos[card][1][1]
+    card1_y, card1_x = card_pos[card_kind][0][0], card_pos[card_kind][0][1]
+    card2_y, card2_x = card_pos[card_kind][1][0], card_pos[card_kind][1][1]
 
-    ry1, rx1, res1 = bfs(sy, sx, left_y, left_x)
-    ry2, rx2, res2 = bfs(ry1, rx1, right_y, right_x)
-    
-    remove(card)
-    backtrack(ry2, rx2, k+1, m, count + res1 + res2)
-    restore(card)
+    # 한 쌍의 카드에서 첫번째 카드를 먼저 지우는 경우이다.
+    ry1, rx1, cnt1 = bfs(board, start_y, start_x, card1_y, card1_x)
+    ry2, rx2, cnt2 = bfs(board, ry1, rx1, card2_y, card2_x)
 
-    ry1, rx1, res1 = bfs(sy, sx, right_y, right_x)
-    ry2, rx2, res2 = bfs(ry1, rx1, left_y, left_x)
+    remove_card(board, card_pos, card_kind)
+    backtracking(board, card_pos, orders, ry2, rx2, rmv_card_num + 1, ord_idx, count + cnt1 + cnt2)
+    restore_card(board, card_pos, card_kind)
 
-    remove(card)
-    backtrack(ry2, rx2, k+1, m, count + res1 + res2)
-    restore(card)
+    # 한 쌍의 카드에서 두번째 카드를 먼저 지우는 경우이다.
+    ry1, rx1, cnt1 = bfs(board, start_y, start_x, card2_y, card2_x)
+    ry2, rx2, cnt2 = bfs(board, ry1, rx1, card1_y, card1_x)
+
+    remove_card(board, card_pos, card_kind)
+    backtracking(board, card_pos, orders, ry2, rx2, rmv_card_num + 1, ord_idx, count + cnt1 + cnt2)
+    restore_card(board, card_pos, card_kind)
+
 
 def solution(board, r, c):
     global answer
-    init(board)
+    # 종류 n인 카드가 저장된 위치를 card_pos[n][0, 1]로 찾을 수 있다.
+    card_pos = defaultdict(list)
 
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] != 0:
+                card_pos[board[i][j]].append([i, j])
+    
+    # 순열을 사용하여 카드를 없애는 순서를 생성한다.
+    orders = list(permutations(card_pos.keys()))
+    
     for i in range(len(orders)):
-        backtrack(r, c, 0, i, 0)
+        backtracking(board, card_pos, orders, r, c, 0, i, 0)
 
     return answer
