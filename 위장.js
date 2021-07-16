11dbb207ec28b2088af5f3b846e9453004a73c8a
Mincheol Seo <smc503@naver.com>
2021-07-16 10:53:17 +0900

    20210716

---
+++ 위장.js
@@ -0,0 +1,20 @@
+function solution(clothes) {
+    let answer = 1;
+    let dict = {};
+
+    for (var i in clothes) {
+        let tmp = clothes[i][1];
+
+        if (tmp in dict) {
+            dict[tmp] += 1;
+        } else {
+            dict[tmp] = 1;
+        }
+    }
+
+    for (var i in dict) {
+        answer *= dict[i] + 1
+    }
+
+    return answer - 1;
+}
