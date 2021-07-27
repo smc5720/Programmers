e5aed9ede8b1c9676685a0497f09f98c5ed592dd
smc5720 <smc5720@gmail.com>
2021-07-27 11:53:31 +0900

    20210727

--- K번째수.js
+++ K번째수.js
@@ -1,4 +1,19 @@
 function solution(array, commands) {
     var answer = [];
+
+    for (var t in commands) {
+        var i = commands[t][0];
+        var j = commands[t][1];
+        var k = commands[t][2];
+
+        var tmp = array.slice(i - 1, j);
+
+        tmp.sort(function (a, b){
+            return a - b;
+        });
+        
+        answer.push(tmp[k - 1]);
+    }
+
     return answer;
 }
