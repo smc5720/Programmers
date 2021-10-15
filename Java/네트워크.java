7082a34d9d3b3c5734c66f0c2e00c178eefbddd0
smc5720 <smc5720@gmail.com>
2021-10-15 17:57:14 +0900

    20211015

--- Java/네트워크.java
+++ Java/네트워크.java
@@ -11,7 +11,7 @@ class Solution {
 		}
 
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+			for (int j = 0; j < n; j++) { 
 				if (i != j) {
 					if (computers[i][j] == 1) {
 						union(parent[i], parent[j]);
