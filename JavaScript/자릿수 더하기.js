daa858f289f7b4d88d899e246cb201525b86c2bd
smc5720 <smc5720@gmail.com>
2022-07-24 20:22:12 +0900

    20220724

--- JavaScript/자릿수 더하기.js
+++ JavaScript/자릿수 더하기.js
@@ -1,15 +1,12 @@
-function logB(x, base) {
-  return Math.log(x) / Math.log(base);
-}
-
 function solution(n) {
   let answer = 0;
-  let size = parseInt(logB(n, 10));
 
-  for (let i = 0; i <= size; i++) {
-    let num = n % 10;
-    n = parseInt(n / 10);
-    answer += num;
+  while (n / 10 != 0 || n % 10 != 0) {
+    let num = parseInt(n / 10);
+    let other = n % 10;
+
+    answer += other;
+    n = num;
   }
 
   return answer;
